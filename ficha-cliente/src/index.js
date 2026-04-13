c2c245bd045292b554b27ff9462d1b1ff7f696f9
copyisabell <copy.isabelle@gmail.com>
2026-04-13 12:31:11 -0300

    Delete ficha-cliente directory

--- ficha-cliente/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import FichaCliente from './FichaCliente';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<FichaCliente />);
